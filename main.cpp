df25bcbb15652ed8fd61ef148a602b6aebcc5a55
ShinKojiro <42621442+ShinKojiro@users.noreply.github.com>
2019-02-13 20:02:22 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,29 +2,38 @@
 
 using namespace std;
 
-void fizz(){
-    cout << "FIZZ";
+void printTitle(int cnt){
+    cout << "You have guessed " << cnt << " times" << endl;
+    cout << "Guess a number between 1 and 100000: " << endl;
 }
-
-void buzz(){
-    cout << "BUZZ";
+    
+bool guess(int num, int cnt){
+    int guessNum;
+    printTitle(cnt);
+    cin >> guessNum;
+    if (guessNum < num){
+        cout << "your number is too low, guess again." << endl;
+        return true;
+    }
+    if (guessNum > num){
+        cout << "your number is too high, guess again." << endl;
+        return true;
+    }
+    if (guessNum == num){
+        cout << "good job, you got it!" << endl;
+        return false;
+    }
+    return 0;
 }
 
 int main(){
-    for(int i = 1; i <= 100; i++){
-        if(i % 3 == 0 && i % 5 == 0){
-            fizz();
-            buzz();
-        }
-        else if(i % 3 == 0){
-            fizz();
-        }
-        else if(i % 5 == 0){
-            buzz();
-        }
-        else{
-            cout << i;
-        }
-        cout  << endl;
+    bool failed = true;
+    int number = 73;
+    int count = 0;
+    
+    while(failed){
+        failed = guess(number, count);
+        count++;  
     }
+    cout << "It took you " << count << " tries."  << endl;
 }
